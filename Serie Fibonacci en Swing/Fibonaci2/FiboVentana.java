160f77784669a009f6eb19b510990a5ca4c4db0c
fabiangarcia017 <fabiangarcia017@gmail.com>
2020-09-25 16:38:35 -0500

    Tarea Finalizada

--- Serie Fibonacci en Swing/Fibonaci2/FiboVentana.java
+++ Serie Fibonacci en Swing/Fibonaci2/FiboVentana.java
@@ -3,6 +3,7 @@ package Fibonaci2;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -11,6 +12,7 @@ import javax.swing.JTextArea;
 import java.awt.Button;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JList;
 
 public class FiboVentana extends JFrame {
 
@@ -48,24 +50,30 @@ public class FiboVentana extends JFrame {
 		contentPane.add(lblNewLabel);
 		
 		JTextArea txtIteraciones = new JTextArea();
-		txtIteraciones.setBounds(89, 28, 62, 22);
+		txtIteraciones.setBounds(118, 28, 62, 22);
 		contentPane.add(txtIteraciones);
 		
 		JLabel lblNewLabel_1 = new JLabel("Numero 1");
-		lblNewLabel_1.setBounds(23, 77, 62, 14);
+		lblNewLabel_1.setBounds(23, 77, 85, 14);
 		contentPane.add(lblNewLabel_1);
 		
 		JTextArea txtNum1 = new JTextArea();
-		txtNum1.setBounds(89, 72, 62, 22);
+		txtNum1.setBounds(118, 72, 62, 22);
 		contentPane.add(txtNum1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Numero 2");
-		lblNewLabel_2.setBounds(23, 121, 46, 14);
+		lblNewLabel_2.setBounds(23, 121, 85, 14);
 		contentPane.add(lblNewLabel_2);
 		
 		JTextArea txtNum2 = new JTextArea();
-		txtNum2.setBounds(89, 116, 62, 22);
+		txtNum2.setBounds(118, 116, 62, 22);
 		contentPane.add(txtNum2);
+		
+		JList Lista = new JList();
+		DefaultListModel modelo = new DefaultListModel();
+		Lista.setBounds(297, 11, 127, 239);
+		contentPane.add(Lista);
+		
 		
 		Button btnCalcularFibonaci = new Button("Calcular Serie Fibonaci");
 		btnCalcularFibonaci.addActionListener(new ActionListener() {
@@ -77,14 +85,20 @@ public class FiboVentana extends JFrame {
 			     do
 			     {
 			          num3=num1+num2;
-			         System.out.println ("interacion "+cont+" "+num3 );
+			         
+			         modelo.addElement("interacion "+cont+"  "+" "+num3);
 			         num1=num2;
 			         num2=num3;
 			         cont += 1;
 			     }while(cont<iteraciones);
+			     Lista.setModel(modelo);
 			}
 		});
-		btnCalcularFibonaci.setBounds(293, 69, 112, 43);
+		btnCalcularFibonaci.setBounds(44, 184, 167, 43);
 		contentPane.add(btnCalcularFibonaci);
+		
+		
+		
+		
 	}
 }
